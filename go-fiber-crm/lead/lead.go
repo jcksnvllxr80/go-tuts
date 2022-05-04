6d56e37fe83cde26a50c69619407ff1e249864fc
Aaron Watkins <ac.watkins80@gmail.com>
2022-05-04 02:40:14 +0000

    move sqlite dir

--- go-fiber-crm/lead/lead.go
+++ go-fiber-crm/lead/lead.go
@@ -1,7 +1,10 @@
 package lead
 
 import (
+	"github.com/jcksnvllxr80/go-tuts/go-fiber-crm/database"
 	"github.com/jinzhu/gorm"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Lead struct {
